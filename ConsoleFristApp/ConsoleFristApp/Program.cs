5876a9c8d4493d2c77b07a453821814ff3141703
Jin-Lee123 <wls4310@koreatech.ac.kr>
2021-05-03 10:34:24 +0900

    First commit

---
+++ ConsoleFristApp/ConsoleFristApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleFristApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");  //주석
+        }
+    }
+}
